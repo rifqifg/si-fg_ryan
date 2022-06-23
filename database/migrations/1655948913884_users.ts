c14e9b90db13f6739bb7d32515b609687509277f
lutfidmz <lutfidmz@gmail.com>
2022-06-23 09:34:13 +0700

    Initial commit

---
+++ database/migrations/1655948913884_users.ts
@@ -0,0 +1,27 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class UsersSchema extends BaseSchema {
+  protected tableName = 'users'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.uuid('id').primary().notNullable()
+      table.string('email', 255).notNullable()
+      table.string('password', 180).notNullable()
+      table.string('remember_me_token').nullable()
+      table.boolean('verified').defaultTo(false)
+      table.string('verify_token').defaultTo(null)
+      table.datetime('verify_expiry')
+      // TODO: lanjut disini, set up email juga belum
+      /**
+       * Uses timestampz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true }).notNullable()
+      table.timestamp('updated_at', { useTz: true }).notNullable()
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}

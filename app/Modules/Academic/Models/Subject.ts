cc2342706e212bc47c8ed9b764cbcd676b916a36
Rizki-SMAFG <rizki.it@smafg.sch.id>
2023-05-15 11:06:28 +0700

    add Subject models and validator

---
+++ app/Modules/Academic/Models/Subject.ts
@@ -0,0 +1,29 @@
+import { DateTime } from 'luxon'
+import { BaseModel, afterCreate, beforeCreate, column } from '@ioc:Adonis/Lucid/Orm'
+import { v4 as uuidv4 } from 'uuid'
+let newId = ""
+
+export default class Subject extends BaseModel {
+  @column({ isPrimary: true })
+  public id: string
+
+  @column()
+  public description: string | null
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+
+  @beforeCreate()
+  public static assignUuid(subject: Subject) {
+    newId = uuidv4()
+    subject.id = newId
+  }
+
+  @afterCreate()
+  public static setNewId(subject: Subject) {
+    subject.id = newId
+  }
+}

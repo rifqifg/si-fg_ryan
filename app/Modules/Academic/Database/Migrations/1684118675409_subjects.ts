ca14b5ea9f15f5d6df46859439a861001717c2a1
RizkiAuliaRamadhan <rwamdhaney2@gmail.com>
2023-05-15 10:32:48 +0700

    add subjects migration

---
+++ app/Modules/Academic/Database/Migrations/1684118675409_subjects.ts
@@ -0,0 +1,28 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'subjects'
+
+  public async up () {
+    this.schema
+    .withSchema('academic')
+    .createTable(this.tableName, (table) => {
+      table.uuid('id').primary().notNullable().unique()
+      table.string('name').notNullable()
+      table.string('description')
+      
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema
+    .withSchema('academic')
+    .dropTable(this.tableName)
+  }
+}

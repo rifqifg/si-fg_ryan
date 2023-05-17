b89bbef4219d01b54fd48c002dd5ac0711b13c15
Rizki-SMAFG <rizki.it@smafg.sch.id>
2023-05-17 09:02:23 +0700

    add migration, models and validator teacher

---
+++ app/Modules/Academic/Models/Teacher.ts
@@ -0,0 +1,17 @@
+import { DateTime } from 'luxon'
+import { BaseModel, HasOne, column, hasOne } from '@ioc:Adonis/Lucid/Orm'
+import Employee from 'App/Models/Employee'
+
+export default class Teacher extends BaseModel {
+  @column({ isPrimary: true })
+  public id: string
+
+  @hasOne(() => Employee)
+  public employee: HasOne<typeof Employee>
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}

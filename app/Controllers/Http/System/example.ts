fcd44027c8e4bbc48d49e67141d004f93d3550f3
lutfidmz <lutfidmz@gmail.com>
2023-05-19 09:28:47 +0700

    hotfix

---
+++ app/Controllers/Http/System/example.ts
@@ -0,0 +1,38 @@
+const data = {
+    key1: 'value',
+    key2: 'value',
+    key3: 'value',
+    key4: 'value',
+    key5: 'value',
+    key6: 'value',
+    key7: 'value',
+    objKey : {
+        key1: 'value',
+        key2: 'value',
+        key3: 'value',
+        key4: 'value',
+        key5: 'value',
+        key6: 'value',
+        key7: 'value',
+    }
+}
+
+const data = {
+    key1: 'value',
+    key2: 'value',
+    key3: 'value',
+    key4: 'value',
+    key5: 'value',
+    key6: 'value',
+    key7: 'value'
+}
+
+const data_2 = {
+    key1: 'value',
+    key2: 'value',
+    key3: 'value',
+    key4: 'value',
+    key5: 'value',
+    key6: 'value',
+    key7: 'value'
+}

f1708870b4335e57b32d092bf6fbdc5fc2bed905
rifqifg <rifqi.it@smafg.sch.id>
2023-05-31 07:46:42 +0700

    Add login, logout and verify email feature for user student candidate

--- app/Modules/PPDB/Controllers/Http/UserStudentCandidatesController.ts
+++ app/Modules/PPDB/Controllers/Http/UserStudentCandidatesController.ts
@@ -1,45 +1,96 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-// import { string } from "@ioc:Adonis/Core/Helpers";
+import { string } from "@ioc:Adonis/Core/Helpers";
 import Env from "@ioc:Adonis/Core/Env";
-
+import Database from "@ioc:Adonis/Lucid/Database";
 import CreateUserStudentCandidateValidator from "../../Validators/CreateUserStudentCandidateValidator";
 import Mail from '@ioc:Adonis/Addons/Mail';
 import UserStudentCandidate from '../../Models/UserStudentCandidate';
+import USCLoginValidator from '../../Validators/USCLoginValidator';
 
 export default class UserStudentCandidatesController {
     public async register({ request, response }: HttpContextContract) {
         let payload = await request.validate(CreateUserStudentCandidateValidator)
 
-        // const verifyToken = string.generateRandom(64);
-        // const FE_URL = Env.get("FE_URL") + verifyToken;
+        const token = string.generateRandom(64)
+        // TODO: change to cyclic
+        const actionUrl = `localhost:3334/ppdb/auth/verify-email?token=${token}`
 
         let user_sc
         try {
-            user_sc = await UserStudentCandidate.create(payload)
+            user_sc = await UserStudentCandidate.create({
+                ...payload,
+                verifyToken: token
+            })
         } catch (error) {
             return response.internalServerError({
-                message: "Gagal input data user calon siswa baru",
+                message: "CO-USC-REG_01: Gagal input data user calon siswa baru",
                 error: error.message
             })
         }
 
-        // TODO: response mail dan validasi user
-        // try {
-        //     await Mail.send((message) => {
-        //         message
-        //             .from(Env.get("SMTP_USERNAME"))
-        //             .to(payload.email)
-        //             .subject("Welcome Onboard!")
-        //         // TODO: html view utk selamat datang
-        //         // .htmlView("emails/registered", { FE_URL });
-        //     })
-        // } catch (error) {
-        //     return response.send({ message: "email tidak valid" });
-        // }
+        try {
+            await Mail.send((message) => {
+                message
+                    .from(Env.get("SMTP_USERNAME"))
+                    .to(payload.email)
+                    .subject("Register Akun PPDB FG Telah Berhasil")
+                    .htmlView("emails/student_candidate_verify_request", { actionUrl })
+            })
+        } catch (error) {
+            return response.send({ message: "email tidak valid" });
+        }
 
         response.ok({
-            message: "Berhasil melakukan register/nSilahkan verifikasi email anda",
+            message: "Berhasil melakukan register, silahkan verifikasi email anda",
             user_sc,
         })
+    }
+
+    public async login({ request, response, auth }: HttpContextContract) {
+        const payload = await request.validate(USCLoginValidator)
+
+        try {
+            const token = await auth.use('ppdb_api').attempt(payload.email, payload.password, {
+                expiresIn: '24 hours'
+            })
+            const user = await UserStudentCandidate.query()
+                .where("id", auth.use('ppdb_api').user!.id)
+
+            response.ok({
+                message: "login successful",
+                token,
+                data: user
+            })
+        } catch (error) {
+            console.log(error)
+
+            return response.badRequest({
+                message: "Invalid credentials",
+                error: error.message
+            });
+        }
+    }
+
+    public async logout({ auth, response }: HttpContextContract) {
+        await auth.use('ppdb_api').revoke()
+        await Database.manager.close("pg")
+        response.ok({ message: "Berhasil logout" })
+    }
+
+    public async verify({ request, response }: HttpContextContract) {
+        const token = request.input("token")
+
+        try {
+            const usc = await UserStudentCandidate.findByOrFail("verifyToken", token)
+
+            await usc.merge({ verifyToken: "", verified: true }).save()
+
+            response.ok({ message: "Akun berhasil diverifikasi" })
+        } catch (error) {
+            return response.badRequest({
+                message: "email tidak ditemukan / token tidak cocok",
+                error: error.message
+            })
+        }
     }
 }
--- app/Modules/PPDB/Routes/ppdb.ts
+++ app/Modules/PPDB/Routes/ppdb.ts
@@ -3,5 +3,9 @@ Route.group(() => {
     Route.get('/', () => {
         return "You got here at ppdb"
     })
-    Route.post('/register', 'UserStudentCandidatesController.register')//.as('ppdb.register')
-}).prefix('ppdbs').namespace('PPDBControllers')
+    Route.post('/register', 'UserStudentCandidatesController.register')
+
+    Route.post('/auth/login', 'UserStudentCandidatesController.login')
+    Route.post('/auth/logout', 'UserStudentCandidatesController.logout').middleware('auth:ppdb_api')
+    Route.get('/auth/verify-email', 'UserStudentCandidatesController.verify')
+}).prefix('ppdb').namespace('PPDBControllers')

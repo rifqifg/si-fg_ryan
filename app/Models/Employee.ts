542bf0eba0c9ff0b8c547abe7a8434619cdf0414
arianurjamal <jamal.psw@gmail.com>
2023-06-05 11:35:43 +0700

    FIX add teacherId on login

--- app/Models/Employee.ts
+++ app/Models/Employee.ts
@@ -1,111 +1,128 @@
-import { DateTime } from 'luxon'
-import { afterCreate, BaseModel, beforeCreate, BelongsTo, belongsTo, column, hasMany, HasMany, ManyToMany, manyToMany } from '@ioc:Adonis/Lucid/Orm'
-import { v4 as uuidv4 } from 'uuid'
-import Division from './Division'
-import Wilayah from './Wilayah'
-import EmployeeDivision from './EmployeeDivision'
-import EmployeeType from './EmployeeType'
-let newId = ""
+import { DateTime } from "luxon";
+import {
+  afterCreate,
+  BaseModel,
+  beforeCreate,
+  BelongsTo,
+  belongsTo,
+  column,
+  hasMany,
+  HasMany,
+  HasOne,
+  hasOne,
+  ManyToMany,
+  manyToMany,
+} from "@ioc:Adonis/Lucid/Orm";
+import { v4 as uuidv4 } from "uuid";
+import Division from "./Division";
+import Wilayah from "./Wilayah";
+import EmployeeDivision from "./EmployeeDivision";
+import EmployeeType from "./EmployeeType";
+import Teacher from "App/Modules/Academic/Models/Teacher";
+let newId = "";
 
 export default class Employee extends BaseModel {
   @column({ isPrimary: true })
-  public id: string
+  public id: string;
 
   @column()
-  public nip: string
+  public nip: string;
 
   @column()
-  public nuptk: string
+  public nuptk: string;
 
   @column()
-  public name: string
+  public name: string;
 
   @column()
-  public birthCity: string
+  public birthCity: string;
 
   @column.date()
-  public birthDay: DateTime
+  public birthDay: DateTime;
 
   @column()
-  public gender: string
+  public gender: string;
 
   @column()
-  public address: string
+  public address: string;
 
   @hasMany(() => EmployeeDivision)
-  public divisions: HasMany<typeof EmployeeDivision>
+  public divisions: HasMany<typeof EmployeeDivision>;
 
   @manyToMany(() => Division, {
-    pivotTable: 'employee_divisions',
-    pivotColumns: ['title']
+    pivotTable: "employee_divisions",
+    pivotColumns: ["title"],
   })
-  public divisi: ManyToMany<typeof Division>
+  public divisi: ManyToMany<typeof Division>;
 
   @column()
-  public employeeTypeId: string
+  public employeeTypeId: string;
 
   @belongsTo(() => EmployeeType)
-  public employeeType: BelongsTo<typeof EmployeeType>
+  public employeeType: BelongsTo<typeof EmployeeType>;
+
+  @hasOne(() => Teacher)
+  public teacher: HasOne<typeof Teacher>;
 
   @column.date()
-  public dateIn: DateTime
+  public dateIn: DateTime;
 
   @column.date()
-  public dateOut: DateTime | null
+  public dateOut: DateTime | null;
 
   @column()
-  kodeProvinsi: string | null
+  kodeProvinsi: string | null;
 
   @belongsTo(() => Wilayah, {
-    localKey: 'kode',
-    foreignKey: 'kodeProvinsi'
+    localKey: "kode",
+    foreignKey: "kodeProvinsi",
   })
-  provinsi: BelongsTo<typeof Wilayah>
+  provinsi: BelongsTo<typeof Wilayah>;
 
   @column()
-  kodeKota: string | null
+  kodeKota: string | null;
 
   @belongsTo(() => Wilayah, {
-    localKey: 'kode',
-    foreignKey: 'kodeKota'
+    localKey: "kode",
+    foreignKey: "kodeKota",
   })
-  kota: BelongsTo<typeof Wilayah>
+  kota: BelongsTo<typeof Wilayah>;
 
   @column()
-  kodeKecamatan: string | null
+  kodeKecamatan: string | null;
 
   @belongsTo(() => Wilayah, {
-    localKey: 'kode',
-    foreignKey: 'kodeKecamatan'
+    localKey: "kode",
+    foreignKey: "kodeKecamatan",
   })
-  kecamatan: BelongsTo<typeof Wilayah>
+  kecamatan: BelongsTo<typeof Wilayah>;
 
   @column()
-  kodeKelurahan: string | null
+  kodeKelurahan: string | null;
 
   @belongsTo(() => Wilayah, {
-    localKey: 'kode',
-    foreignKey: 'kodeKelurahan'
+    localKey: "kode",
+    foreignKey: "kodeKelurahan",
   })
-  kelurahan: BelongsTo<typeof Wilayah>
+  kelurahan: BelongsTo<typeof Wilayah>;
 
   @column({ serializeAs: null })
-  public rfid: string | null
+  public rfid: string | null;
 
   @column.dateTime({ autoCreate: true })
-  public createdAt: DateTime
+  public createdAt: DateTime;
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
-  public updatedAt: DateTime
+  public updatedAt: DateTime;
 
   @beforeCreate()
   public static assignUuid(employee: Employee) {
-    newId = uuidv4()
-    employee.id = newId
+    newId = uuidv4();
+    employee.id = newId;
   }
 
   @afterCreate()
   public static setNewId(employee: Employee) {
-    employee.id = newId
+    employee.id = newId;
   }
 }

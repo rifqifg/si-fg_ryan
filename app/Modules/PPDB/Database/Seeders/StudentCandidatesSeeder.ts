351ceb651394d286bf040de04a1c14aab45fcc1d
rifqifg <rifqi.it@smafg.sch.id>
2023-06-07 11:29:17 +0700

    Fix student candidate seeder

--- app/Modules/PPDB/Database/Seeders/StudentCandidatesSeeder.ts
+++ app/Modules/PPDB/Database/Seeders/StudentCandidatesSeeder.ts
@@ -9,12 +9,14 @@ export default class extends BaseSeeder {
       {
         id: "a3760e48-3f86-4b78-9a8d-c0a6c40e3a0d",
         userId: "a9d7c2f6-6b84-4c7e-9c2f-82aefc325e1d",
-        registrationId: "special_reg_id"
+        registrationId: "special_reg_id",
+        fullName: "Rel Mayer"
       },
       {
         id: "c5e9b8d7-09b1-4b89-8bc5-61a297fd3d8a",
         userId: "7d36f602-efdc-43c9-8ce0-6a8c5a305f7b",
-        registrationId: "special_reg_id2"
+        registrationId: "special_reg_id2",
+        fullName: "Arsy Berlian"
       }
     ]
 

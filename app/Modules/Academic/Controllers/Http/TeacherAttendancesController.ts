db0dfef95e49ffd17b9ebbaf814ca51bb9933b06
Rizki-SMAFG <rizki.it@smafg.sch.id>
2023-06-09 08:56:54 +0700

    show, update, delete teacherAttendance

--- app/Modules/Academic/Controllers/Http/TeacherAttendancesController.ts
+++ app/Modules/Academic/Controllers/Http/TeacherAttendancesController.ts
@@ -3,6 +3,8 @@ import CreateTeacherAttendanceValidator from '../../Validators/CreateTeacherAtte
 import TeacherAttendance from '../../Models/TeacherAttendance'
 const luxon_1 = require("luxon")
 const hariIni = luxon_1.DateTime.now().toSQLDate().toString()
+import { validate as uuidValidation } from "uuid"
+import UpdateTeacherAttendanceValidator from '../../Validators/UpdateTeacherAttendanceValidator'
 
 export default class TeacherAttendancesController {
 
@@ -42,6 +44,7 @@ export default class TeacherAttendancesController {
             })
         }
     }
+
     public async store({ request, response }: HttpContextContract) {
         const payload = await request.validate(CreateTeacherAttendanceValidator)
         try {
@@ -57,4 +60,71 @@ export default class TeacherAttendancesController {
             })
         }
     }
+
+    public async show({ params, response }: HttpContextContract) {
+        const { id } = params
+        if (!uuidValidation(id)) { return response.badRequest({ message: "DailyAttendance ID tidak valid" }) }
+
+        try {
+            const data = await TeacherAttendance
+                .query()
+                .preload('teacher', s => s.preload('employee', e => e.select('name')))
+                .preload('class', c => c.select('name'))
+                .preload('session', s => s.select('session'))
+                .preload('subject', s => s.select('name'))
+                .where('id', id).firstOrFail()
+            response.ok({ message: "Berhasil mengambil data", data })
+        } catch (error) {
+            const message = "ACTA-SHOW: " + error.message || error
+            console.log(error);
+            response.badRequest({
+                message: "Gagal mengambil data",
+                error: message,
+                error_data: error
+            })
+        }
+    }
+
+    public async update({ params, request, response }: HttpContextContract) {
+        const { id } = params
+        if (!uuidValidation(id)) { return response.badRequest({ message: "TeacherAttendance ID tidak valid" }) }
+    
+        const payload = await request.validate(UpdateTeacherAttendanceValidator)
+        if (JSON.stringify(payload) === '{}') {
+          console.log("data update kosong");
+          return response.badRequest({ message: "Data tidak boleh kosong" })
+        }
+        try {
+          const daily = await TeacherAttendance.findOrFail(id)
+          const data = await daily.merge(payload).save()
+          response.ok({ message: "Berhasil mengubah data", data })
+        } catch (error) {
+          const message = "ACTA-UPDATE: " + error.message || error
+          console.log(error);
+          response.badRequest({
+            message: "Gagal mengubah data",
+            error: message,
+            error_data: error
+          })
+        }
+      }
+    
+      public async destroy({ params, response }: HttpContextContract) {
+        const { id } = params
+        if (!uuidValidation(id)) { return response.badRequest({ message: "TeacherAttendance ID tidak valid" }) }
+    
+        try {
+          const data = await TeacherAttendance.findOrFail(id)
+          await data.delete()
+          response.ok({ message: "Berhasil menghapus data" })
+        } catch (error) {
+          const message = "ACTA-DESTROY: " + error.message || error
+          console.log(error);
+          response.badRequest({
+            message: "Gagal menghapus data",
+            error: message,
+            error_data: error
+          })
+        }
+      }
 }

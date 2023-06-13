cbec8593a2d2200e7a08ffa6887d282d9b7aa9b3
rifqifg <rifqi.it@smafg.sch.id>
2023-06-13 14:14:03 +0700

    Add file upload method and validator for candidate parent

--- app/Modules/PPDB/Controllers/Http/StudentCandidateParentsController.ts
+++ app/Modules/PPDB/Controllers/Http/StudentCandidateParentsController.ts
@@ -1,9 +1,12 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import StudentCandidateParent from '../../Models/StudentCandidateParent'
 import { validate as uuidValidation } from 'uuid'
+import Env from "@ioc:Adonis/Core/Env"
+import Drive from '@ioc:Adonis/Core/Drive'
 import StudentCandidate from '../../Models/StudentCandidate'
 import CreateStudentCandidateParentValidator from '../../Validators/CreateStudentCandidateParentValidator'
 import UpdateStudentCandidateParentValidator from '../../Validators/UpdateStudentCandidateParentValidator'
+import ScParentFileUploadValidator from '../../Validators/ScParentFileUploadValidator'
 
 export default class StudentCandidateParentsController {
     public async index({ request, response, params }: HttpContextContract) {
@@ -75,6 +78,35 @@ export default class StudentCandidateParentsController {
             console.log(error)
             response.badRequest({ message: "Gagal mengubah data", error: error.message })
         }
+    }
+
+    public async fileUpload({ request, response, params }: HttpContextContract) {
+        const { id } = params
+        if (!uuidValidation(id)) { return response.badRequest({ message: "ID ortu calon siswa tidak valid" }) }
+
+        const payload = await request.validate(ScParentFileUploadValidator)
+        const imageName = `candidate_parent_${id}_${payload.category}.${payload.file.extname}`
+        const data = await StudentCandidateParent.findOrFail(id)
+
+        await payload.file.moveToDisk(
+            'student-candidate-parents',
+            { name: imageName, overwrite: true },
+            'ppdb'
+        )
+
+        const beHost = Env.get('BE_URL')
+        const drivePpdb = Drive.use('ppdb')
+        const imageUrl = beHost + await drivePpdb.getUrl('student-candidate-parents/' + imageName)
+
+        if (payload.category === 'ktp') {
+            await data.merge({ ktpScan: imageName }).save()
+        }
+
+        response.ok({
+            message: "Upload Success",
+            data,
+            image_url: imageUrl
+        })
     }
 
     public async destroy({ params, response }: HttpContextContract) {

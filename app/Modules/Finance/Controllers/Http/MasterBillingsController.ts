49542301a356265c8000f536fef4555b41ea837f
Ryan Rifqi <rifqi.it@smafg.sch.id>
2023-08-16 10:29:22 +0700

    Add show, update and delete route for MasterBilling

--- app/Modules/Finance/Controllers/Http/MasterBillingsController.ts
+++ app/Modules/Finance/Controllers/Http/MasterBillingsController.ts
@@ -1,6 +1,8 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import MasterBilling from '../../Models/MasterBilling';
 import CreateMasterBillingValidator from '../../Validators/CreateMasterBillingValidator';
+import { validate as uuidValidation } from "uuid";
+import UpdateMasterBillingValidator from '../../Validators/UpdateMasterBillingValidator';
 
 export default class MasterBillingsController {
   public async index({ request, response }: HttpContextContract) {
@@ -39,6 +41,67 @@ export default class MasterBillingsController {
         message: "Gagal menyimpan data",
         error: message,
       })
+    }
+  }
+
+  public async show({ params, response }: HttpContextContract) {
+    const { id } = params;
+    if (!uuidValidation(id)) { return response.badRequest({ message: "ID tidak valid" })}
+
+    try {
+      const data = await MasterBilling.findOrFail(id)
+      response.ok({ message: "Berhasil mengambil data", data });
+    } catch (error) {
+      const message = "FMB-SHO: " + error.message || error;
+      console.log(error);
+      response.badRequest({
+        message: "Gagal mengambil data",
+        error: message,
+        error_data: error,
+      });
+    }
+  }
+
+  public async update({ params, request, response }: HttpContextContract) {
+    const { id } = params;
+    const payload = await request.validate(UpdateMasterBillingValidator);
+    if (JSON.stringify(payload) === "{}") {
+      return response.badRequest({ message: "Data tidak boleh kosong" });
+    }
+
+    try {
+      const masterBilling = await MasterBilling.findOrFail(id);
+      const data = await masterBilling.merge(payload).save();
+      response.ok({ message: "Berhasil mengubah data", data });
+    } catch (error) {
+      const message = "FMB-UPD: " + error.message || error;
+      console.log(error);
+      response.badRequest({
+        message: "Gagal mengubah data",
+        error: message,
+        error_data: error,
+      });
+    }
+  }
+
+  public async destroy({ params, response }: HttpContextContract) {
+    const { id } = params;
+    if (!uuidValidation(id)) {
+      return response.badRequest({ message: "ID tidak valid" });
+    }
+
+    try {
+      const data = await MasterBilling.findOrFail(id);
+      await data.delete();
+      response.ok({ message: "Berhasil menghapus data" });
+    } catch (error) {
+      const message = "FMB-DES: " + error.message || error;
+      console.log(error);
+      response.badRequest({
+        message: "Gagal menghapus data",
+        error: message,
+        error_data: error,
+      });
     }
   }
 }

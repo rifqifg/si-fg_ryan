7b41a59c8619a1421e8569d3cac3cbc2c0b8ecf0
Ryan Rifqi <rifqi.it@smafg.sch.id>
2023-08-17 14:01:45 +0700

    Create BREAD routes for Accounts

--- app/Modules/Finance/Routes/finance.ts
+++ app/Modules/Finance/Routes/finance.ts
@@ -6,5 +6,6 @@ Route.group(() => {
 
     Route.resource('master-billings', 'MasterBillingsController').apiOnly().middleware({ '*': ['auth'] })
     Route.resource('billings', 'BillingsController').apiOnly().middleware({ '*': ['auth'] })
+    Route.resource('accounts', 'AccountsController').apiOnly().middleware({ '*': ['auth'] })
 
 }).prefix('finance').namespace('FinanceControllers')

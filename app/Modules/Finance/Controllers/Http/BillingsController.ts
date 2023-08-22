6359918b3817c9d30fbfe2dd97dbe9b2f1898af0
Ryan Rifqi <rifqi.it@smafg.sch.id>
2023-08-22 13:46:24 +0700

    Add batch create endpoint for billings

--- app/Modules/Finance/Controllers/Http/BillingsController.ts
+++ app/Modules/Finance/Controllers/Http/BillingsController.ts
@@ -1,5 +1,6 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Billing from '../../Models/Billing';
+import CreateBillingValidator from '../../Validators/CreateBillingValidator';
 
 export default class BillingsController {
   public async index({ request, response }: HttpContextContract) {
@@ -15,12 +16,29 @@ export default class BillingsController {
         data = await Billing.query().whereILike('name', `%${keyword}%`)
       }
 
-      response.ok({ message: "Berhasil mengambil data", data });
+      response.ok({ message: "Berhasil mengambil data", data })
     } catch (error) {
       const message = "FBIL-INDEX: " + error.message || error;
       console.log(error);
       response.badRequest({
         message: "Gagal mengambil data",
+        error: message,
+        error_data: error,
+      });
+    }
+  }
+
+  public async store({ request, response }: HttpContextContract) {
+    const payload = await request.validate(CreateBillingValidator)
+
+    try {
+      const data = await Billing.createMany(payload.billings)
+      response.created({ message: "Berhasil menyimpan data", data })
+    } catch (error) {
+      const message = "FBIL-STORE: " + error.message || error;
+      console.log(error);
+      response.badRequest({
+        message: "Gagal menyimpan data",
         error: message,
         error_data: error,
       });
---
+++ app/Modules/Finance/Validators/CreateBillingValidator.ts
@@ -0,0 +1,28 @@
+import { schema, CustomMessages, rules } from '@ioc:Adonis/Core/Validator'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { BillingType } from '../lib/enums'
+
+export default class CreateBillingValidator {
+  constructor(protected ctx: HttpContextContract) { }
+
+  public schema = schema.create({
+    billings: schema.array().members(
+      schema.object().members({
+        account_id: schema.string({}, [
+          rules.exists({ table: 'finance.accounts', column: 'id' })
+        ]),
+        master_billing_id: schema.string({}, [
+          rules.exists({ table: 'finance.master_billings', column: 'id' })
+        ]),
+        name: schema.string(),
+        amount: schema.string([
+          rules.regex(new RegExp("^[1-9][0-9]*$")),
+        ]),
+        description: schema.string.optional(),
+        type: schema.enum.optional(Object.values(BillingType))
+      })
+    )
+  })
+
+  public messages: CustomMessages = {}
+}

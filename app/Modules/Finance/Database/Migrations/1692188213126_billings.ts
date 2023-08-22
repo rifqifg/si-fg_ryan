49e11cf13cdf493a9db36276e1bc716415927a0d
Ryan Rifqi <rifqi.it@smafg.sch.id>
2023-08-22 11:00:20 +0700

    Update billings migration

--- app/Modules/Finance/Database/Migrations/1692188213126_billings.ts
+++ app/Modules/Finance/Database/Migrations/1692188213126_billings.ts
@@ -9,13 +9,13 @@ export default class extends BaseSchema {
       .withSchema('finance')
       .createTable(this.tableName, (table) => {
         table.uuid('id').primary().notNullable().unique()
-        table.uuid('student_id').references('id').inTable('academic.students').onDelete('set null').onUpdate('cascade')
+        table.uuid('account_id').references('id').inTable('finance.accounts').onDelete('set null').onUpdate('cascade')
         table.uuid('master_billing_id').references('id').inTable('finance.master_billings').onDelete('set null').onUpdate('cascade')
         table.string('name').notNullable()
         table.string('amount').notNullable()
         table.string('description')
         table.enum('status', Object.values(BillingStatus)).notNullable()
-        table.enum('type', Object.values(BillingType)).notNullable()
+        table.enum('type', Object.values(BillingType))
 
         /**
          * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL

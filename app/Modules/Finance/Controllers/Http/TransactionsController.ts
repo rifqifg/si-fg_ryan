2d57aa599e38813cdbd77a4f679394b11f129eb4
Ryan Rifqi <rifqi.it@smafg.sch.id>
2023-08-25 14:24:24 +0700

    Add show, update, delete endpoint for transactions

--- app/Modules/Finance/Controllers/Http/TransactionsController.ts
+++ app/Modules/Finance/Controllers/Http/TransactionsController.ts
@@ -1,6 +1,8 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Transaction from '../../Models/Transaction';
 import CreateTransactionValidator from '../../Validators/CreateTransactionValidator';
+import { validate as uuidValidation } from "uuid";
+import UpdateTransactionValidator from '../../Validators/UpdateTransactionValidator';
 
 export default class TransactionsController {
   public async index({ request, response }: HttpContextContract) {
@@ -44,6 +46,79 @@ export default class TransactionsController {
         message: "Gagal menyimpan data",
         error: message,
       })
+    }
+  }
+
+  public async show({ params, response }: HttpContextContract) {
+    const { id } = params;
+
+    if (!uuidValidation(id)) {
+      return response.badRequest({ message: "ID tidak valid" });
+    }
+
+    try {
+      const data = await Transaction.query()
+        .where('id', id)
+        .preload('billing', qBilling => {
+          qBilling
+            .select('account_id')
+            .preload('account', qAccount => {
+            qAccount.select('account_name', 'number')
+          })
+        })
+        .firstOrFail()
+      response.ok({ message: "Berhasil mengambil data", data });
+    } catch (error) {
+      const message = "FTR-SHO: " + error.message || error;
+      console.log(error);
+      response.badRequest({
+        message: "Gagal mengambil data",
+        error: message,
+        error_data: error,
+      })
+    }
+  }
+
+  public async update({ params, request, response }: HttpContextContract) {
+    const { id } = params;
+    const payload = await request.validate(UpdateTransactionValidator);
+    if (JSON.stringify(payload) === "{}") {
+      return response.badRequest({ message: "Data tidak boleh kosong" });
+    }
+
+    try {
+      const transaction = await Transaction.findOrFail(id);
+      const data = await transaction.merge(payload).save();
+      response.ok({ message: "Berhasil mengubah data", data });
+    } catch (error) {
+      const message = "FTR-UPD: " + error.message || error;
+      console.log(error);
+      response.badRequest({
+        message: "Gagal mengubah data",
+        error: message,
+        error_data: error,
+      });
+    }
+  }
+
+  public async destroy({ params, response }: HttpContextContract) {
+    const { id } = params;
+    if (!uuidValidation(id)) {
+      return response.badRequest({ message: "ID tidak valid" });
+    }
+
+    try {
+      const data = await Transaction.findOrFail(id);
+      await data.delete();
+      response.ok({ message: "Berhasil menghapus data" });
+    } catch (error) {
+      const message = "FMB-DES: " + error.message || error;
+      console.log(error);
+      response.badRequest({
+        message: "Gagal menghapus data",
+        error: message,
+        error_data: error,
+      });
     }
   }
 }

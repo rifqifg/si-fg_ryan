efe96d1a71381a4797285cab3eb7605a860a4a79
Ryan Rifqi <rifqi.it@smafg.sch.id>
2023-09-11 09:35:19 +0700

    Add function for spreadsheet import

--- app/Modules/Finance/Controllers/Http/AccountsController.ts
+++ app/Modules/Finance/Controllers/Http/AccountsController.ts
@@ -3,6 +3,13 @@ import Account from '../../Models/Account';
 import CreateAccountValidator from '../../Validators/CreateAccountValidator';
 import { validate as uuidValidation } from "uuid";
 import UpdateAccountValidator from '../../Validators/UpdateAccountValidator';
+import UploadSpreadsheetAccountValidator from '../../Validators/UploadSpreadsheetAccountValidator';
+import fs from "fs";
+import XLSX from "xlsx";
+import Student from 'App/Modules/Academic/Models/Student';
+import CreateManyAccountValidator from '../../Validators/CreateManyAccountValidator';
+import { HttpContext } from '@adonisjs/core/build/standalone';
+import { validator } from '@ioc:Adonis/Core/Validator'
 
 export default class AccountsController {
   public async index({ request, response }: HttpContextContract) {
@@ -47,7 +54,7 @@ export default class AccountsController {
 
   public async show({ params, response }: HttpContextContract) {
     const { id } = params;
-    if (!uuidValidation(id)) { return response.badRequest({ message: "ID tidak valid" })}
+    if (!uuidValidation(id)) { return response.badRequest({ message: "ID tidak valid" }) }
 
     try {
       const data = await Account.findOrFail(id)
@@ -104,5 +111,66 @@ export default class AccountsController {
         error_data: error,
       });
     }
+  }
+
+  public async import({ request, response }: HttpContextContract) {
+    let payload = await request.validate(UploadSpreadsheetAccountValidator)
+
+    const excelBuffer = fs.readFileSync(payload.upload.tmpPath?.toString()!);
+    const jsonData = await AccountsController.spreadsheetToJSON(excelBuffer)
+
+    if (jsonData == 0) response.badRequest({ message: "Data tidak boleh kosong" })
+
+    const wrappedJson = { "accounts": jsonData }
+    const manyAccountValidator = new CreateManyAccountValidator(HttpContext.get()!, wrappedJson)
+    const payloadAccount = await validator.validate(manyAccountValidator)
+
+    try {
+      const data = await Account.createMany(payloadAccount.accounts)
+
+      response.created({ message: "Berhasil import data", data })
+    } catch (error) {
+      const message = "FAC-IMP: " + error.message || error;
+      response.badRequest({
+        message: "Gagal import data",
+        error: message,
+      })
+    }
+  }
+
+  // helper function utk import excel + convert ke json
+  private static async spreadsheetToJSON(excelBuffer) {
+    let workbook = await XLSX.read(excelBuffer)
+
+    // Mendapatkan daftar nama sheet dalam workbook
+    const sheetNames = workbook.SheetNames
+
+    // membaca isi dari sheet pertama
+    const firstSheet = workbook.Sheets[sheetNames[0]]
+    const jsonData: Array<object> = XLSX.utils.sheet_to_json(firstSheet)
+
+    if (jsonData.length <= 1) return 0
+
+    // Warning: async call didalam loop (map)
+    // might refactor later
+    const formattedJson = await Promise.all(jsonData.map(async data => {
+      const nisSiswa = data["NIS"].toString()
+
+      const student = await Student.query()
+        .where('nis', '=', nisSiswa)
+        .firstOrFail()
+      const accountName = `Rekening ${data['Jenis Akun']} ${student.name}`
+      const balance = data["Saldo"] ? data["Saldo"].toString() : "0"
+
+      return {
+        coa_id: data['Nomor COA'].toString(),
+        student_id: student.id,
+        account_name: accountName,
+        balance: balance,
+        number: data['Nomor Rekening'].toString()
+      }
+    }))
+
+    return formattedJson
   }
 }

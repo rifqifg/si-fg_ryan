676d3afe81cd60e2ae8be587b8d591f8b7eb8785
Ryan Rifqi <rifqi.it@smafg.sch.id>
2023-09-11 09:34:22 +0700

    Add new spreadsheet template for accounts

--- database/seeders/TemplateExcel.ts
+++ database/seeders/TemplateExcel.ts
@@ -6,12 +6,18 @@ export default class extends BaseSeeder {
     // Write your database queries inside the run method
     console.log(">>> START seeding table: template_excels")
 
-    await TemplateExcel.createMany([
+    await TemplateExcel.fetchOrCreateMany('name', [
       {
         id: "3c54a7c6-c953-41f3-99b8-d06723da3820",
         name: "Template Import Data Siswa",
         link: "https://docs.google.com/spreadsheets/d/1ZW7u7eYoLwfL5oMxXPw93Em1hORFxq-0/export",
         description: "Template untuk impor data siswa"
+      },
+      {
+        id: "7c110b95-5e6d-4c11-9b23-dc44ebef4a84",
+        name: "Template Import Data Rekening Siswa",
+        link: "https://docs.google.com/spreadsheets/d/1-DhiSLzVzCzm72blxToVB9MQKmwdd0L8qjYPmB5u6-U/export",
+        description: "Template untuk impor data rekening siswa"
       }
     ])
 

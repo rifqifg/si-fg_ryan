d6cdc04d0f0b5acde7c1c79c1366a873836e648d
Rizki-SMAFG <rizki.it@smafg.sch.id>
2023-09-12 11:08:13 +0700

    rekap absensi aktifitas tidak tetap

--- start/routes.ts
+++ start/routes.ts
@@ -82,3 +82,4 @@ Route.shallowResource('sub-activities', 'SubActivitiesController').apiOnly().mid
 Route.get('presences', 'SubActivitiesController.getPresenceSubActivity').middleware(['auth', 'checkRole:admin'])
 Route.post('presences/:activityId/:subActivityId', 'SubActivitiesController.presence').middleware(['auth', 'checkRole:admin'])
 Route.delete('multi-delete-presences', 'SubActivitiesController.destroyPresences').middleware(['auth', 'checkRole:admin'])
+Route.get('recap-sub-activities/:activityId', 'SubActivitiesController.recap').middleware(['auth', 'checkRole:admin'])

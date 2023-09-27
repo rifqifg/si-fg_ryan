4e4120dbef6d008ac2819dc3a6f7f06e20993868
Rizki-SMAFG <rizki.it@smafg.sch.id>
2023-09-27 09:03:35 +0700

    make migration monthly report

---
+++ database/migrations/1695772853164_monthly_reports.ts
@@ -0,0 +1,24 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'monthly_reports'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.uuid('id').unique().notNullable().primary()
+      table.string('name').notNullable()
+      table.date('fromDate').notNullable()
+      table.date('toDate').notNullable()
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}

1d7dafda76e78fd4dd9916ca8ee2e55a4b925d97
Rizki-SMAFG <rizki.it@smafg.sch.id>
2023-11-24 09:07:28 +0700

    BREAD assessmentComponent

--- app/Controllers/Http/AssessmentComponentsController.ts
+++ app/Controllers/Http/AssessmentComponentsController.ts
@@ -2,7 +2,10 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import AssessmentComponent from 'App/Models/AssessmentComponent'
 import { CreateRouteHist } from 'App/Modules/Log/Helpers/createRouteHist'
 import { statusRoutes } from 'App/Modules/Log/lib/enum'
+import CreateAssessmentComponentValidator from 'App/Validators/CreateAssessmentComponentValidator'
+import UpdateAssessmentComponentValidator from 'App/Validators/UpdateAssessmentComponentValidator'
 import { DateTime } from 'luxon'
+import { validate as uuidValidation } from "uuid"
 
 export default class AssessmentComponentsController {
   public async index({ request, response }: HttpContextContract) {
@@ -31,5 +34,103 @@ export default class AssessmentComponentsController {
     }
   }
 
-  //TODO: CRUD
+  public async store({ request, response }: HttpContextContract) {
+    const dateStart = DateTime.now().toMillis()
+    CreateRouteHist(statusRoutes.START, dateStart)
+    const payload = await request.validate(CreateAssessmentComponentValidator)
+
+    try {
+      const data = await AssessmentComponent.create(payload);
+      CreateRouteHist(statusRoutes.FINISH, dateStart)
+      response.created({ message: "Berhasil menyimpan data", data });
+    } catch (error) {
+      const message = "HRDAC02: " + error.message || error;
+      CreateRouteHist(statusRoutes.ERROR, dateStart, message)
+      console.log(error);
+      response.badRequest({
+        message: "Gagal mengambil data",
+        error: message,
+        error_data: error,
+      });
+    }
+  }
+
+  public async show({ params, response }: HttpContextContract) {
+    const dateStart = DateTime.now().toMillis()
+    CreateRouteHist(statusRoutes.START, dateStart)
+    const { id } = params;
+    if (!uuidValidation(id)) {
+      return response.badRequest({ message: "AssessmentComponent ID tidak valid" });
+    }
+
+    try {
+      const data = await AssessmentComponent.query().where("id", id).firstOrFail();
+      CreateRouteHist(statusRoutes.FINISH, dateStart)
+      response.ok({ message: "Berhasil mengambil data", data });
+    } catch (error) {
+      const message = "HRDAC03: " + error.message || error;
+      CreateRouteHist(statusRoutes.ERROR, dateStart, message)
+      console.log(error);
+      response.badRequest({
+        message: "Gagal mengambil data",
+        error: message,
+        error_data: error,
+      });
+    }
+  }
+
+  public async update({ params, request, response }: HttpContextContract) {
+    const dateStart = DateTime.now().toMillis()
+    CreateRouteHist(statusRoutes.START, dateStart)
+    const { id } = params;
+    if (!uuidValidation(id)) {
+      return response.badRequest({ message: "AssessmentComponent ID tidak valid" });
+    }
+
+    const payload = await request.validate(UpdateAssessmentComponentValidator);
+    if (JSON.stringify(payload) === "{}") {
+      console.log("data update kosong");
+      return response.badRequest({ message: "Data tidak boleh kosong" });
+    }
+    try {
+      const assessmentComponent = await AssessmentComponent.findOrFail(id);
+      const data = await assessmentComponent.merge(payload).save();
+      CreateRouteHist(statusRoutes.FINISH, dateStart)
+      response.ok({ message: "Berhasil mengubah data", data });
+    } catch (error) {
+      const message = "HRDLE04: " + error.message || error;
+      CreateRouteHist(statusRoutes.ERROR, dateStart, message)
+      console.log(error);
+      response.badRequest({
+        message: "Gagal mengubah data",
+        error: message,
+        error_data: error,
+      });
+    }
+  }
+
+  public async destroy({ params, response }: HttpContextContract) {
+    const dateStart = DateTime.now().toMillis()
+    CreateRouteHist(statusRoutes.START, dateStart)
+    const { id } = params;
+    if (!uuidValidation(id)) {
+      return response.badRequest({ message: "AssessmentComponent ID tidak valid" });
+    }
+
+    try {
+      const data = await AssessmentComponent.findOrFail(id);
+      await data.delete();
+      CreateRouteHist(statusRoutes.FINISH, dateStart)
+      response.ok({ message: "Berhasil menghapus data" });
+    } catch (error) {
+      const message = "HRDAC05: " + error.message || error;
+      CreateRouteHist(statusRoutes.ERROR, dateStart, message)
+      console.log(error);
+      response.badRequest({
+        message: "Gagal menghapus data",
+        error: message,
+        error_data: error,
+      });
+    }
+  }
 }

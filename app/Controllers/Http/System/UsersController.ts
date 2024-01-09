4c59dd305b237d62eeee68f6f2795b1de2310412
Ryan Rifqi <rifqi.it@smafg.sch.id>
2024-01-09 10:24:59 +0700

    Coba fix register pls

--- app/Controllers/Http/System/UsersController.ts
+++ app/Controllers/Http/System/UsersController.ts
@@ -481,7 +481,10 @@ export default class UsersController {
       return response.internalServerError({ message: "Gagal mengirim email verifikasi", error: error.message });
     }
 
+    console.log(payload.role === ROLE.TEACHER);
+
     if (payload.role === ROLE.EMPLOYEE || payload.role === ROLE.TEACHER) {
+      console.log("lucu");
       try {
         employee = await Employee.findByOrFail("nik", payload.nik);
       } catch (error) {
@@ -500,6 +503,7 @@ export default class UsersController {
         roleName: ROLE.EMPLOYEE
       })
     } else {
+      console.log("HAHA!");
       try {
         student = await Student.findByOrFail("nisn", payload.nisn);
       } catch (error) {

f1f4c2660661a43af6231e0cbffbb0c56a0567d1
Rizki Aulia Ramadhan <rizkiauliaramadhan@192.168.1.12>
2024-01-13 09:41:31 +0700

    fix triwulanEmployeeDetail validator

--- app/Controllers/Http/TriwulanEmployeeDetailsController.ts
+++ app/Controllers/Http/TriwulanEmployeeDetailsController.ts
@@ -1,15 +1,10 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import TriwulanEmployeeDetail from 'App/Models/TriwulanEmployeeDetail';
-import { CreateRouteHist } from 'App/Modules/Log/Helpers/createRouteHist';
-import { statusRoutes } from 'App/Modules/Log/lib/enum';
 import UpdateTriwulanEmployeeDetailValidator from 'App/Validators/UpdateTriwulanEmployeeDetailValidator';
-import { DateTime } from 'luxon';
 import { validate as uuidValidation } from "uuid"
 
 export default class TriwulanEmployeeDetailsController {
   public async update({ params, request, response }: HttpContextContract) {
-    const dateStart = DateTime.now().toMillis()
-    CreateRouteHist(statusRoutes.START, dateStart)
     const { id } = params;
     if (!uuidValidation(id)) {
       return response.badRequest({ message: "TriwulanEmployeeDetails ID tidak valid" });
@@ -26,7 +21,6 @@ export default class TriwulanEmployeeDetailsController {
       response.ok({ message: "Berhasil mengubah data", data });
     } catch (error) {
       const message = "HRDTWED01: " + error.message || error;
-      CreateRouteHist(statusRoutes.ERROR, dateStart, message)
       console.log(error);
       response.badRequest({
         message: "Gagal mengambil data",

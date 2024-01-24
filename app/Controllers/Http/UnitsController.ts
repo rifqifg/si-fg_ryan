d76f30c749fd9a67587473ad0ec925bd2409a441
Ryan Rifqi <rifqi.it@smafg.sch.id>
2024-01-24 12:36:06 +0700

    Add filter by employee_id in getUnit

--- app/Controllers/Http/UnitsController.ts
+++ app/Controllers/Http/UnitsController.ts
@@ -149,7 +149,7 @@ export default class UnitsController {
   }
 
   public async getUnit({ request, response, auth }: HttpContextContract) {
-    const { keyword = "" } = request.qs()
+    const { keyword = "", employee_id } = request.qs()
 
     try {
       const unitIds = await unitHelper()
@@ -160,6 +160,10 @@ export default class UnitsController {
         .preload('employeeUnits', eu => eu.where('title', 'lead').andWhere('employee_id', auth.user!.$attributes.employeeId))
         .if(!superAdmin, query => {
           query.whereIn('id', unitIds)
+          query.whereHas('employeeUnits', eu => {
+            eu.where('title', 'lead')
+            .if(employee_id, e => e.andWhere('employee_id', employee_id))
+          })
         })
 
       response.ok({ message: "get data successfully", data })

ef7d5c8dc5e48d2205fe53c5ee17f7c60f361a61
unknown <rizki.it@smafg.sch.id>
2024-02-21 09:00:35 +0700

    fix migration foundation

--- app/Modules/Foundation/Database/Migrations/1707963480856_foundations.ts
+++ app/Modules/Foundation/Database/Migrations/1707963480856_foundations.ts
@@ -16,6 +16,14 @@ export default class extends BaseSchema {
       table.timestamp('created_at', { useTz: true })
       table.timestamp('updated_at', { useTz: true })
     })
+
+    //create first data
+    this.schema.raw(`
+      INSERT INTO foundation.foundations
+      (id, name, description, created_at, updated_at)
+      VALUES
+      ('c1d3e93b-1774-4682-9a81-c1915742c8e2', 'FG Putra', 'SMA FG', now(), now())
+    `)
   }
 
   public async down() {

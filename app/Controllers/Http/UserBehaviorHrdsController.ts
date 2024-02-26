0fdb16eb212468c403c8e69741d0ac3b77234078
unknown <rizki.it@smafg.sch.id>
2024-02-26 08:50:26 +0700

    feat: filter by foundation in user behavior

--- app/Controllers/Http/UserBehaviorHrdsController.ts
+++ app/Controllers/Http/UserBehaviorHrdsController.ts
@@ -1,5 +1,4 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import { checkRoleSuperAdmin } from 'App/Helpers/checkRoleSuperAdmin'
 import { RolesHelper } from 'App/Helpers/rolesHelper'
 import { unitHelper } from 'App/Helpers/unitHelper'
 import Activity from 'App/Models/Activity'
@@ -12,38 +11,46 @@ export default class UserBehaviorHrdsController {
   public async activity({ response, auth }: HttpContextContract) {
     const dateStart = DateTime.now().toMillis()
     CreateRouteHist(statusRoutes.START, dateStart)
-    const user = await User.query().preload('roles', r => r.preload('role')).where('id', auth.use('api').user!.id).firstOrFail()
+    const user = await User.query()
+      .preload('employee', e => e
+        .select('id', 'name', 'foundation_id'))
+      .preload('roles', r => r
+        .preload('role'))
+      .where('employee_id', auth.user!.$attributes.employeeId)
+      .first()
     const userObject = JSON.parse(JSON.stringify(user))
 
-    const superAdmin = await checkRoleSuperAdmin()
     // const unitIds = await unitHelper()
     const unitLeadIds = await unitHelper("lead")
     const roles = await RolesHelper(userObject)
     const isAdminHrd = roles.includes('admin_hrd')
 
     let data: object
-    if (superAdmin) {
+    if (roles.includes('super_admin') || roles.includes('admin_foundation')) {
       data = await Activity.query()
         .preload('unit', unit => unit.select('id', 'name'))
         .preload('categoryActivity', categoryActivity => categoryActivity.select('id', 'name'))
+        .if(roles.includes('admin_foundation'), query => {
+          query.orWhereHas('unit', u => u.where('foundation_id', user!.employee.foundationId))
+        })
     } else {
       data = await Activity.query()
         .preload('unit', unit => unit.select('id', 'name'))
         .preload('categoryActivity', categoryActivity => categoryActivity.select('id', 'name'))
-          .whereHas('activityMembers', am => {
-            am.where('employee_id', user.employeeId)
-              .andWhere('role', 'manager')
-          })
-          .if((isAdminHrd), q => q.orWhereIn('unit_id', unitLeadIds))
-        // .andWhere(query => {
-        //   query.whereHas('activityMembers', am => (am.where('employee_id', user.employeeId), am.where('role', 'manager')))
-        //     .if(isAdminHrd, query => {
-        //       query.orWhereHas('unit', u => u
-        //         .whereIn('id', unitIds)
-        //         .andWhere(query => query.whereHas('employeeUnits', eu => eu.where('title', 'lead'))))
-        //     })
-        // })
-        // .andWhereIn('unit_id', unitIds)
+        .whereHas('activityMembers', am => {
+          am.where('employee_id', user!.employeeId)
+            .andWhere('role', 'manager')
+        })
+        .if((isAdminHrd), q => q.orWhereIn('unit_id', unitLeadIds))
+      // .andWhere(query => {
+      //   query.whereHas('activityMembers', am => (am.where('employee_id', user.employeeId), am.where('role', 'manager')))
+      //     .if(isAdminHrd, query => {
+      //       query.orWhereHas('unit', u => u
+      //         .whereIn('id', unitIds)
+      //         .andWhere(query => query.whereHas('employeeUnits', eu => eu.where('title', 'lead'))))
+      //     })
+      // })
+      // .andWhereIn('unit_id', unitIds)
     }
 
     CreateRouteHist(statusRoutes.FINISH, dateStart)
